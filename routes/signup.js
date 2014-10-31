7b94fa8f84e2fa4d6fecdd21ed099ca9b7867a42
ronaldohoch <ronaldopintohoch@gmail.com>
2014-10-31 16:33:11 -0200

    Primeiro Commit
    Primeiro Commit

---
+++ routes/signup.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+var middlewere = function(req,res,next){
+	console.log("Bombo");
+	next();
+}
+
+
+/* GET users listing. */
+router.get('/', function(req, res) {
+  // res.send('Cadastro');
+  res.render('signup', { title: 'Signup',msg:'Bem vindo.' });
+});
+
+/* GET users listing. */
+router.post('/new', middlewere, function(req, res) {
+  res.json({'Novo':"new"});
+  // res.render('signup', { title: 'Signup',msg:'Bem vindo.' });
+});
+
+module.exports = router;
