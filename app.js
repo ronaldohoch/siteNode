78d2f97c0097b51896c5d5773a11d1ab595ad99b
ronaldohoch <ronaldopintohoch@gmail.com>
2014-11-06 17:52:49 -0200

    CRUD :O *-*

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongo = require("mongodb");
+var monk = require("monk");
+var db = monk('localhost:27017/siteNode');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -11,18 +14,26 @@ var signup = require('./routes/signup');
 
 var app = express();
 
+console.log("dirname:",__dirname);
+
+//https://www.npmjs.org/package/consolidate
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-//https://www.npmjs.org/package/consolidate
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Faz o banco ficar acessivel para o router
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
--- routes/signup.js
+++ routes/signup.js
@@ -1,10 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var middlewere = function(req,res,next){
-	console.log("Bombo");
-	next();
-}
-
 
 /* GET users listing. */
 router.get('/', function(req, res) {
@@ -13,14 +8,46 @@ router.get('/', function(req, res) {
 });
 
 /* GET users listing. */
-router.post('/new', middlewere, function(req, res) {
-  res.json({'Novo':"new"});
-  // res.render('signup', { title: 'Signup',msg:'Bem vindo.' });
+router.post('/new', function(req, res) {
+  var db = req.db;
+
+  //Pega os valores por qualquer meio que vier, seja ele POST ou GET
+  var userName = req.param('name'),
+      password = req.param('password');
+  //Seleciona a collection para inserir
+  var collection = db.get('Users');
+  //Envia para o banco de dados
+  collection.insert({
+  	"name":userName,
+  	"senha":password
+  },function(err,doc){
+  	if(err){
+  		//Se falhar, retorna o erro.
+  		res.send("Deu erro.");
+  	}else{
+  		//Se funcionar, troca a página
+  		res.location("userlist");
+  		//mostra a página de userlist
+  		res.redirect("userlist");
+  	}
+  })
 });
 
-router.get('/delete', middlewere, function(req, res) {
+router.get('/delete', function(req, res) {
   res.send("Deletado");
   // res.render('signup', { title: 'Signup',msg:'Bem vindo.' });
 });
+
+//Pega a lista de usuários
+router.get('/userlist',function(req,res){
+	var db = req.db;
+	var collection = db.get('Users');
+	collection.find({},{},function(e,docs){
+		console.log("e,docs:",e,docs);
+		res.render('userlist',{
+			"userlist":docs
+		});
+	});
+})
 
 module.exports = router;
